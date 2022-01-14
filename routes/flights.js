9793b433cd271c4ef94fb67453eea18fddd9be9f
mehjwazir <mehjwazir@gmail.com>
2022-01-13 20:44:57 -0800

    update

---
+++ routes/flights.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var flightsCtrl = require('../controllers/flights');
+
+
+
+
+// All routes start with "/flights"
+//Index GET/ Flights
+router.get('/', flightsCtrl.index);
+
+//New GET /flights/new
+
+router.get('/new', flightsCtrl.new);
+
+// Create POST/ flights
+router.post('/', flightsCtrl.create)
+
+
+
+module.exports = router;
